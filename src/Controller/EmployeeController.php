7cf564e4109c610df38ab5ed9df724feb316be23
Damir Alenov <alenodam@fit.cvut.cz>
2024-10-29 16:13:19 +0100

    add bonus

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -5,24 +5,33 @@ namespace App\Controller;
 use App\Repository\EmployeeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class EmployeeController extends AbstractController
 {
-    private $employeeRepository;
+    private EmployeeRepository $employeeRepository;
+    private int $itemsPerPage = 5;
 
     public function __construct(EmployeeRepository $employeeRepository)
     {
         $this->employeeRepository = $employeeRepository;
     }
 
-    #[Route(path: '/employees', name: 'employees')]
-    public function index(): Response
+    #[Route(path: '/employees/{page}', name: 'employees', requirements: ['page' => '\d+'], defaults: ['page' => 1])]
+    public function index(int $page = 1): Response
     {
-        $employees = $this->employeeRepository->findAll();
+        $offset = ($page - 1) * $this->itemsPerPage;
+
+        $employees = $this->employeeRepository->findBy([], null, $this->itemsPerPage, $offset);
+
+        $totalEmployees = $this->employeeRepository->count([]);
+        $totalPages = ceil($totalEmployees / $this->itemsPerPage);
 
         return $this->render('employee/index.html.twig', [
-            'employees' => $employees
+            'employees' => $employees,
+            'currentPage' => $page,
+            'totalPages' => $totalPages,
         ]);
     }
 }
