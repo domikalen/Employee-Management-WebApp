01ad17084e6fc419b50cc3e127bec19bbf2c50fb
Damir Alenov <alenodam@fit.cvut.cz>
2024-11-07 22:45:20 +0100

    hw06

--- migrations/Version20241107211942.php
+++ migrations/Version20241107211942.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241107202429 extends AbstractMigration
+final class Version20241107211942 extends AbstractMigration
 {
     public function getDescription(): string
     {
